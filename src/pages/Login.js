0b5111bae47b4cc40852bbba31fbd9dfbc1c41cf
Sthephany Granados <lsgranados04@gmail.com>
2020-07-22 18:10:35 -0500

    Se agregan vistas de Start y Login

---
+++ src/pages/Login.js
@@ -0,0 +1,24 @@
+const Login = () => {
+  const view = `
+    <div class = "login">
+      <img src = "LOGO" alt = "Logo phySport" class = "login__logo">
+      <h3 class = "login__title>Inicia sesion</h3>
+        <form id = "loginForm" class = "form">
+          <input type = "email" id = "loginEmail" class = "form__email" placeholder = "Email" autofocus>   
+          <input type = "password" id = "loginPassword" class = "form__password" placeholder = "Password"> 
+          <button id = "loginButton" class = "form__button">
+            Ingresar
+          </button>
+        </form>
+      <p class = "login__text"> o inicia sesion con 
+        <button class = "button__google></button>
+      </p>
+      <p class = "login__text"> Nuevo usuario?
+        <a href = "#/">Registrate</a>
+      </p>
+    </div>
+  `;
+  return view;
+};
+
+export default Login;
--- src/pages/start.js
+++ src/pages/start.js
@@ -1,1 +1,11 @@
+const Start = () => {
+  const view = `
+    <div class = "start">
+      <img src = "LOGO" alt = "Logo phySport" class = "start__logo">
+      <h3 class = "start__slogan">Find a game, win friends</h3>
+    </div>
+  `;
+  return view;
+};
 
+export default Start;
