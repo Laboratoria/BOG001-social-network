0a9617d315b3d82eb0fcdb11c14baf43ae443475
Dulce Mercado <soydulceangelina@gmail.com>
2020-07-22 13:36:43 -0500

    webpack dependecies, basic structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-// Este es el punto de entrada de tu aplicacion
+// // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+import './styles/styles.scss';
