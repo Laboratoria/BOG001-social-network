2b9bb9e8025363abddead9c1580a9afaa6f9b71a
Sthephany Granados <lsgranados04@gmail.com>
2020-07-23 09:47:39 -0500

    Estandarizar codigo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,21 @@
 const login = () => {
   const view = `
-    <div class = "login">
-      <img src = "LOGO" alt = "Logo phySport" class = "login__logo">
-      <h3 class = "login__title>Inicia sesion</h3>
-        <form id = "loginForm" class = "form">
+    <section class = "login">      
+      <h3 class = "login__title"> Inicia sesion</h3>
+      <form id = "loginForm" class = "form">
           <input type = "email" id = "loginEmail" class = "form__email" placeholder = "Email" autofocus>   
           <input type = "password" id = "loginPassword" class = "form__password" placeholder = "Password"> 
           <button href = "#/.." id = "loginButton" class = "form__button">
             Ingresar
           </button>
         </form>
-      <p class = "login__text"> o inicia sesion con 
-        <button class = "button__google></button>
-      </p>
-      <p class = "login__text"> Nuevo usuario?
-        <a href = "#/..">Registrate</a>
-      </p>
-    </div>
+      <h3 class = "login__text"> o inicia sesion con 
+        <a href="#/..." id="" class = "button__google">G</a>
+      </h3>
+      <h3 class = "login__text"> ¿Nuevo usuario?
+        <a href = "#/.." id="" class="">Registrate</a>
+      </h3>
+    </section>
   `;
   return view;
 };
--- src/pages/start.js
+++ src/pages/start.js
@@ -1,7 +1,7 @@
 const start = () => {
   const view = `
     <section class = "start">
-      <img src = "LOGO" alt = "Logo phySport" class = "start__logo">
+      <img src = "../assets/logoWhite.png" alt = "Logo phySport" class = "start__logo">
       <h3 class = "start__slogan">Find a game, win friends</h3>
     </section>
   `;
