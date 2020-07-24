4fc0bca31e960e3113f5c19e7cc46de2d7cc8cb1
Oriana <oriana.nohemi22@gmail.com>
2020-07-24 09:35:26 -0500

    cconfiguracion de firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB8mjbw1ZN7VfV-Ejeb8-Wqz4aXt2HXgdY',
+  authDomain: 'physport-dfb88.firebaseapp.com',
+  databaseURL: 'https://physport-dfb88.firebaseio.com',
+  projectId: 'physport-dfb88',
+  storageBucket: 'physport-dfb88.appspot.com',
+  messagingSenderId: '580315971427',
+  appId: '1:580315971427:web:05abf4935feae5648ddd56',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
