75235b0b16170d86b0c0763d1a86c6640c861803
Dulce Mercado <soydulceangelina@gmail.com>
2020-07-25 15:33:24 -0500

    styles in singUp

--- src/pages/singUp.js
+++ src/pages/singUp.js
@@ -1,24 +1,21 @@
 const singUp = () => {
   const view = `
-    <section class='singUp'>
-      <h1 class='singUp__title'>
-          Registrate
-      </h1>      
-      <form class='singUp__form'>
-          <input id='' type='text' placeholder='username'>
-          <input id='' type='email' placeholder='correo'>
-          <input id='' type='text' placeholder='Ciudad'>
-          <input id='' type='password' placeholder='contraseña'>
-          <input id='' type='password' placeholder='verificar contraseña'>
-          <button id='' type='submit'><a href='#/...'>Registrar</a></button>
-      </form>
-      <div class='singUp__google'>
-          <h3>o registrate con</h3>
-          <h3> 
-              <a href='#/...'>G</a>
-          </h3>
-      </div>
-    </section>`;
+  <section class='singUp'>
+  <h1 class='singUp__title'>
+      Registrate
+  </h1>      
+  <form class='singUp__form'>
+      <input id='' type='text' placeholder='username'>
+      <input id='' type='email' placeholder='correo'>
+      <input id='' type='text' placeholder='ciudad'>
+      <input id='' type='password' placeholder='contraseña'>
+      <button id='' type='submit'>Registrar</button>
+  </form>
+  <div class='singUp__google'>
+      <h3>o registrate con</h3>
+      <a href='#/...'><img class="google-icon" src="./assets/seo-and-web.png" alt=""></a>
+  </div>
+</section>`;
   return view;
 };
 
