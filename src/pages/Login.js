70ca77f6112a35970d6aad2f9764b5e0e4247ddd
Sthephany Granados <lsgranados04@gmail.com>
2020-07-26 21:33:47 -0500

    ajuste para estilos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,23 @@
 const login = () => {
   const view = `
-    <section class = "login">      
-      <h3 class = "login__title"> Inicia sesion</h3>
+    <section class = "login">
+      <div class="login__container">
+        <img src = "./assets/logoWhite.png" alt = "Logo phySport" class = "login__logo">
+        <h3 class = "login__title"> Inicia sesión</h3>
+      </div>
       <form id = "loginForm" class = "form">
-          <input type = "email" id = "loginEmail" class = "form__email" placeholder = "Email" autofocus>   
-          <input type = "password" id = "loginPassword" class = "form__password" placeholder = "Password"> 
-          <button href = "#/.." id = "loginButton" class = "form__button">
-            Ingresar
-          </button>
-        </form>
-      <div class="">
-        <h3 class = "login__text"> o inicia sesion con 
-          <a href="#/..." id="" class = "button__google">G</a>
-        </h3>
-      </div>
-      <div class="">
-        <h3 class = "login__text"> ¿Nuevo usuario?
-          <a href = "#/.." id="" class="">Registrate</a>
-        </h3>
-      </div>
+        <input type = "email" id = "loginEmail" class = "form__email" placeholder = "Email" autofocus>   
+        <input type = "password" id = "loginPassword" class = "form__password" placeholder = "Password"> 
+        <button href = "#/.." id = "loginButton" class = "form__button">
+          Ingresar
+        </button>
+      </form>    
+      <h3 class = "login__text"> o inicia sesión con 
+        <a href="#/..." id="" class = "login__google">G</a>
+      </h3>     
+      <h3 class = "login__text"> ¿Nuevo usuario?
+        <a href = "#/.." id="" class="login__register">Registrate</a>
+      </h3>    
     </section>
   `;
   return view;
--- src/pages/start.js
+++ src/pages/start.js
@@ -1,8 +1,10 @@
 const start = () => {
   const view = `
     <section class = "start">
-      <img src = "../assets/logoWhite.png" alt = "Logo phySport" class = "start__logo">
-      <h3 class = "start__slogan">Find a game, win friends</h3>
+      <div class="start__container">
+        <img src = "./assets/logoWhite.png" alt = "Logo phySport" class = "start__logo">
+        <h4 class = "start__slogan">Find a game, win friends</h4>
+      </div>
     </section>
   `;
   return view;
