3c141b383f27f3df0bffa1bd8f76bfe1247444d7
Sthephany Granados <lsgranados04@gmail.com>
2020-07-29 21:03:44 -0500

    pruebas pantalla start

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import router from './routes/router';
-// import start from './pages/start';
+import start from './pages/start';
 
 import './firebase/init';
 import './styles/styles.scss';
 
 const root = document.getElementById('root');
 
+root.innerHTML = start();
+
 window.addEventListener('load', () => {
   root.insertAdjacentElement('beforeend', router(window.location.hash));
 });
+
+// setTimeout(() => {
+// }, 4000);
 
 window.addEventListener('hashchange', () => {
   root.innerHTML = '';
