be8db00986bf594e1e4d3c1914107d27c5da55bc
Sthephany Granados <lsgranados04@gmail.com>
2020-08-05 10:27:37 -0500

    Prueba mostrar evento en timeline

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -3,25 +3,23 @@ import { getEvents } from '../firebase/post';
 function timeline(valor) {
   const listElement = document.createElement('li');
   listElement.innerHTML = `
-  <h1>HOLAAAAAAAAA</H1>
-  <p>${valor}</p>
-  <p>${valor}</p>`;
+    <h1>HOLAAAAAAAAA</H1>
+    <p>${valor.lugar}</p>
+    <p>${valor.descripcion}</p>`;
   console.log(valor);
   return listElement;
 }
+// console.log(timeline());
 
 const exportData = async () => {
   const querySnapshot = await getEvents();
   querySnapshot.forEach((doc) => {
-    const data = doc.data().lugar;
-    timeline(data);
+    const data = doc.data();
+    console.log(timeline(data));
   });
 };
 
-
-// console.log(timeline());
 exportData();
+export default timeline;
 
 // console.log(exportData());
-
-export default timeline;
