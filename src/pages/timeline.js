9290db8f430505d1ce2da5a1e5ed1617f90341f8
Oriana <oriana.nohemi22@gmail.com>
2020-08-05 14:59:14 -0500

    timeline estilos de los eventos

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -4,20 +4,21 @@ const event = (evento) => {
   const eventContainer = document.createElement('article');
   eventContainer.setAttribute('class', 'event');
   eventContainer.innerHTML = `
+  <a href="#/event"><span id="newEvent" class="flaticon-edit icons postIcon"></span></a>
     <div class="event__info">
       <div class="event__upper--container">
         <div class="user">
           <img src="../assets/perfil.png">
-          <h2></h2>
+          <h2>${evento.nombre}</h2>
         </div>
         <div class="sport">
           <img src="../assets/balon.png">
-          <span>HORA</span>
-          <span>FECHA</span>
+          <span>${evento.hora}</span>
+          <span>${evento.fechaEvento}</span>
         </div>
       </div>
       <p><span class="event__subtitle">Lugar:</span>${evento.lugar}</p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat eaque ipsum non fugit aspernatur praesentium ab qui dolorem.</p>
+      <p>${evento.descripcion}</p>
     </div>
     <div class="event__interaction">
       <div>
@@ -28,14 +29,29 @@ const event = (evento) => {
         <span class="flaticon-speech-bubble icons"></span>
         <span>Comentar</span>
       </div>
+      <div>
+      <div class="flaticon-menu icons">
+        <ul class="hide eventList">
+          <li>
+            Editar Evento
+          </li>
+          <li>
+            Eliminar Evento
+          </li>
+        </ul>
+      </div>
+    </div>
     </div>
   `;
+  eventContainer.querySelector('.flaticon-menu').addEventListener('click', () => eventContainer.querySelector('ul').classList.toggle('hide'));
+
   return eventContainer;
 };
 
 const timeline = async () => {
   const container = document.createElement('section');
   container.setAttribute('class', 'timeline-container');
+
 
   const exportData = async () => {
     const querySnapshot = await getEvents();
