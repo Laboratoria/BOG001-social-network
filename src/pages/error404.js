632d763ae34fe1581444465612fd4dd403688133
Sthephany Granados <lsgranados04@gmail.com>
2020-08-08 15:13:46 -0500

    Estilos error404

--- src/pages/error404.js
+++ src/pages/error404.js
@@ -1,8 +1,11 @@
 const error404 = () => {
   const view = `
-  <h1>
-    Error 404
-  </h1>
+    <section class="error404">
+      <div class="error404-img">
+        <img src="../assets/img-error404.png" alt="">
+      </div>
+      <h1 class="error404-title">Opss! Error 404</h1>      
+    </section>  
   `;
   const container = document.createElement('div');
   container.innerHTML = view;
