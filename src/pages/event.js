aa8c810514dd47f5c0ecb067f69b1f339af1d34b
Oriana <oriana.nohemi22@gmail.com>
2020-08-09 00:02:15 -0500

    nombres de clases, agrega label y placeholder

--- src/pages/event.js
+++ src/pages/event.js
@@ -2,48 +2,47 @@ import { saveEvent } from '../firebase/post';
 
 const event = () => {
   const view = `
-  <section class = "event">
   <a href="#/timeline"><span class="flaticon-remove postIcon"></span></a>
     <h1 class="login__title container__form--title">Crea tu evento</h1>
-    <form class="event_form form" id="event-form" action = "" method = "">    
-      <div class="">
-        <label for = "fname" class="">Fecha</label>
-        <input  type="date" id="fecha" name="Fecha" required autocomplete = "off" >
+    <form class="event_form form" id="event-form">
+      <div class="form-group">
+      <label for="date">Fecha</label>
+      <input type="date" id="date" required autocomplete = "off" >
       </div>
-      <div class="">
-        <label for = "Hora" class="">Hora</label>    
-        <input class="" type = "time" id = "hora" name  = "hora" required autocomplete = "off" >
-      </div>      
-      <div>
-        <label class="" for = "fname">Deporte</label>
-        <select class="" type = "text" id = "deporte" name = "Deporte" required autocomplete = "off" >
-          <option class="" value="Futbol">Fútbol</option>
-          <option class="" value="Baloncesto">Baloncesto</option>
-          <option class="" value="Senderismo">Senderismo</option>
-          <option class="" value="Béisbol">Béisbol</option>
-          <option class="" value="Ciclismo">Ciclismo</option>
+      <div class="form-group">
+        <label for="time" class="">Hora</label>
+        <input type="time" id="time" required autocomplete = "off" >
+      </div>
+      <div class="form-group">
+        <label for="sport">Deporte</label>
+        <select type="text" id="sport" required autocomplete = "off" >
+          <option value="Futbol">Fútbol</option>
+          <option value="Baloncesto">Baloncesto</option>
+          <option value="Senderismo">Senderismo</option>
+          <option value="Béisbol">Béisbol</option>
+          <option value="Ciclismo">Ciclismo</option>
         </select>
-      </div>      
-      <div class="">
-        <textarea name="place" id="place" cols="35" rows="3" maxlength="80" placeholder="Lugar, maximo 80 caracteres" required></textarea>        
       </div>
-      <div class="">
-        <textarea name="description" id="description" cols="35" rows="8" maxlength="150" placeholder="Descripcion maximo 150 caracteres" required></textarea>        
-      </div>      
-      <div class="">
-        <button type="submit" class="button" id = "publicar">Publicar</button>
+      <div class="form-group">
+        <label for="place">Lugar</label>
+        <textarea name="place" id="place" cols="35" rows="3" maxlength="80" placeholder="Kr 28c, Calle 20, Cancha de Futbol Es un Ejemplo, Localidad elEjemplo. <br> Maximo 80 caracteres" required></textarea>        
       </div>
+      <div class="form-group">
+      <label for="description">Descripcion</label>
+        <textarea name="description" id="description" cols="35" rows="8" maxlength="150" placeholder="Jugaremos futbol en la cancha a las 6:00pm, nos faltan 3 jugadores. Traer agua y mucho animo. <br> Descripcion maximo 150 caracteres" required></textarea>        
+      </div>      
+        <button type="submit" class="button" id="publicar">Publicar</button>
     </form>  
-  </section>
   `;
-  const container = document.createElement('div');
+  const container = document.createElement('section');
+  container.setAttribute('class', 'event');
   container.innerHTML = view;
   const eventForm = container.querySelector('#event-form');
 
   const createEvent = () => {
-    const hour = document.getElementById('hora').value;
-    const date = document.getElementById('fecha').value;
-    const sport = document.getElementById('deporte').value;
+    const hour = document.getElementById('time').value;
+    const date = document.getElementById('date').value;
+    const sport = document.getElementById('sport').value;
     const place = document.getElementById('place').value;
     const description = document.getElementById('description').value;
 
