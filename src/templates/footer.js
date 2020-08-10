bfbe562314a915249a0035ab47573212c057b63b
Sthephany Granados <lsgranados04@gmail.com>
2020-08-10 11:12:00 -0500

    Unir cambios

---
+++ src/templates/footer.js
@@ -0,0 +1,12 @@
+const footerTemplate = () => {
+  const view = `
+  <footer class="footer">
+    <div class="footerInfo">
+      <p>Derechos reservados, hecho con amor 💙</p>
+      <p>© Physport 2020</p>
+    </div>
+  </footer>
+  `;
+  return view;
+};
+export default footerTemplate;
