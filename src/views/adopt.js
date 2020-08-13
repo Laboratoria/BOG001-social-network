6ad8c280f5897c51841e55f393eb756f7ae267f3
Dani0210 <alejandrad087@gmail.com>
2020-08-13 17:40:27 -0500

    Maquetacion inicial

---
+++ src/views/adopt.js
@@ -0,0 +1,11 @@
+const adopt = () => {
+    const view =`
+    <div class="adopt">
+      <img src="./src/images/logo.png" alt="logo" class="logo-adopt">
+                <img src=".src/images/logo.png" alt="home-adopt" class="home-adopt">
+                //Aqui iria el post linkeado
+            <a href="#" class="link-adopt">adopt</a>
+        <img src="./src/images/cat-adopt.png" alt="adopt-cat" class="cat-adopt">
+    </div>
+    `;
+}
---
+++ src/views/error404.js
@@ -0,0 +1,13 @@
+const error404 = ()=>{
+    const view =`
+    <div class="server-error">
+        <img src="./src/images/image-error" alt="error404" class="image-error">
+        <h1 class="error404">
+          The Page You Requestd Could Not Be Found
+        </h1>
+    </div>
+    `;
+    return error404();
+    
+}
+export default error404;
