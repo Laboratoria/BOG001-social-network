296aff4fd36b26c4ecfa0b770243152389c97c37
Dani0210 <alejandrad087@gmail.com>
2020-08-13 10:06:59 -0500

    Botones iniciales

---
+++ src/form-register.js
@@ -0,0 +1,5 @@
+const signupFormButton=document.createElement('button')
+signupFormButton.type ='button';
+signupFormButton.innerText= 'Sign Up';
+signupFormButton.classList.add('signupFormButton')
+ document.body.appendChild(signupFormButton);
---
+++ src/initial-session.js
@@ -0,0 +1,14 @@
+ 
+ 
+ const loginButton=document.createElement('button')
+ loginButton.type ='button';
+ loginButton.innerText='LOGIN';
+ loginButton.classList.add('loginButton');
+ document.body.appendChild(loginButton);
+  
+ const registerButton=document.createElement('button')
+ registerButton.type ='button';
+ registerButton.innerText= 'Register';
+ registerButton.classList.add('registerButton');
+ document.body.appendChild(registerButton);
+ 
