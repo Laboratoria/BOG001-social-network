bea8d52636a2fb640214355f1de5c1245293048f
aguevarab0729 <angelica0729@gmail.com>
2020-08-22 18:24:53 -0500

    cambios publicaciones y main

--- src/vistas/inicio.js
+++ src/vistas/inicio.js
@@ -1,16 +1,35 @@
 const showFirst = () => {
     let elementContainer= document.getElementById('container');
 elementContainer.innerHTML =
-`<header>
+`<style>
+body {
+    background-image: linear-gradient(to top, #37ecba 0%, #72afd3 100%);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+}    
+</style> 
+<header>
 <nav class="topNav" id="inicio">
     <ul>
         <li><a href="#/"><span class="fas fa-home"></span>Inicio</a></li>
         <li><a href="#/publicaciones"><span class="far fa-images"></span>Publicaciones</a></li>
         <li><a href="#/perfil"><span class="far fa-user-circle"></span>Perfil</a></li>
-        <li><a href="" onclick="closeSession()"><span class="far fa-times-circle"></span>Cerrar sesión</a></li>
+    </ul>
+    <ul class="btnCloseSession">
+    <li><a href="" onclick="closeSession()"><span class="far fa-times-circle"></span>Cerrar sesión</a></li></li>
     </ul>   
 </nav>
-</header>`
+</header>
+<main>
+<section>
+<h1>¿Cómo podrías sacarle provecho a esta red social?</h1>
+<p>Por medio de “” podrás hacer parte de una comunidad compuesta por amantes de la aventura que aman viajar, hacer turismo y lo mejor: Compartir sus experiencias y recomendaciones, lo que le permitirá elegir la mejor opción para sus próximas vacaciones, basadas en sugerencias de hoteles, actividades, gastronomía, y sitios mas representativos del lugar con su respectivo precio.</p>
+<img src="img/colombiaturismo.jpg">
+</section>
+</main>
+`
 ;
 };
 
--- src/vistas/registro.js
+++ src/vistas/registro.js
@@ -1,30 +1,39 @@
 const showLogin = () => {
     let elementContainer= document.getElementById('container');
 elementContainer.innerHTML =
-`<form class="registro" id="registro">
-        <h2>Usuarios Registrados</h2>
-        <button id="btninicio-sesion">Iniciar Sesión</button>
-        <h2><strong>Registrese</strong></h2>
-        <p class="fas fa-exclamation-triangle" id='errorMessage'></p>
-        <p>Complete este formulario para crear una cuenta</p>
-        <label for="email"><b>Correo Electronico :</b></label><br>
-        <input type="text" id="input-correo" class="registro-input" placeholder="ingresa correo" size="20" required>
-        <label for="psw"><b>Contraseña : </b></label><br>
-        <input type="password" id="input-psw1" class="registro-input" placeholder="***********" size="20" required>
-        <label for="psw-repeat"><b>Repite la Contraseña :</b></label>
-        <input type="password" id="input-psw2" class="registro-input" placeholder="***********" size="20" required>
-        <p>Al crear una cuenta, acepta nuestros <a href="#" style="color:dodgerblue"><br>Terminos y condiciones de privacidad</a>.</p>
-        <div class="btnRegistro">
-            <button type="button" class="cancelarbtn">Cancelar</button>
-            <button type="submit" class="registrarse">Registrate</button>
-        </div>    
-    </form>
+`<style>
+body {
+    background-image: url(img/faro.jpg);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+}
+</style>
+<form class="registro" id="registro">
+    <h2>Usuarios Registrados</h2>
+    <button id="btninicio-sesion">Iniciar Sesión</button>
+    <h2><strong>Registrese</strong></h2>
+    <p class="fas fa-exclamation-triangle" id='errorMessage'></p>
+    <p>Complete este formulario para crear una cuenta</p>
+    <label for="email"><b>Correo Electronico :</b></label><br>
+    <input type="text" id="input-correo" class="registro-input" placeholder="ingresa correo" size="20" required>
+    <label for="psw"><b>Contraseña : </b></label><br>
+    <input type="password" id="input-psw1" class="registro-input" placeholder="***********" size="20" required>
+    <label for="psw-repeat"><b>Repite la Contraseña :</b></label>
+    <input type="password" id="input-psw2" class="registro-input" placeholder="***********" size="20" required>
+    <p>Al crear una cuenta, acepta nuestros <a href="#" style="color:dodgerblue"><br>Terminos y condiciones de privacidad</a>.</p>
+    <div class="btnRegistro">
+        <button type="button" class="cancelarbtn">Cancelar</button>
+        <button type="submit" class="registrarse">Registrate</button>
+    </div>    
+</form>
 
 <div class="modal">
     <div class="modal-content">
         <span class="close">&times;</span>
         <form id="Login" class= "inicio-sesion">
-        <p class="messageSing"></p>
+            <p class="messageSing"></p>
             <input type="text" name="username" id="login-email" placeholder="Correo Electronico" required>
             <input type="password" id= "login-psw"name="password" placeholder="Contraseña" required>
             <input type="submit" class="login" value="Iniciar Sesión">
@@ -36,4 +45,3 @@ elementContainer.innerHTML =
 };
 
 export default showLogin;
-
