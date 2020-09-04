dcea18dfe6da7077849c8b08918db87dfdc7f2b3
aguevarab0729 <angelica0729@gmail.com>
2020-09-04 09:28:49 -0500

    un poco de css topnav y publicaciones

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -25,50 +25,48 @@ export const createPost = () => {
         .catch(function(error) {console.error("Error adding document: ", error);
         });
     })
-        
+    
     firebase.firestore().collection("publications").onSnapshot((querySnapshot) => {
-        let contenedor = document.getElementById('containerPost');
-        contenedor.innerHTML = '';
-        querySnapshot.forEach((doc) => {
-        const id = doc.id;
-        //console.log(id);
-        //console.log(typeof doc.data().createdAt);
-        contenedor.innerHTML += 
-        `<div class = "cardPost">
-        <h1>${doc.data().title}</h1>
-        <p>${doc.data().description}</p>
-        <img src="${doc.data().url}">
-        <br>
-        <i class="fas fa-heart" id="btn-like"></i>
-        <p><span id="mostrar"></span> me gusta</p>
-        <button class="btn-edition" data-id="${id}">Editar</button>
-        <button class="btn-delete" data-id="${id}">Eliminar</button>
-        <p>${doc.data().createdAt.toDate()}</p>
-        </div>`
+    let contenedor = document.getElementById('containerPost');
+    contenedor.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+    const id = doc.id;
+    //console.log(id);
+    //console.log(typeof doc.data().createdAt);
+    contenedor.innerHTML += 
+    `<div class = "cardPost">
+    <h1>${doc.data().title}</h1>
+    <p>${doc.data().description}</p>
+    <img src="${doc.data().url}"> 
+    <br>
+    <i class="fas fa-heart" id="btn-like"></i>
+    <p><span id="mostrar"></span> me gusta</p>
+    <button class="btn-edition" data-id="${id}">Editar</button>
+    <button class="btn-delete" data-id="${id}">Eliminar</button>
+    <p>${doc.data().createdAt.toDate()}</p>
+    </div>`
+    //Likes publicación
+    const likes = () => {
+        let contador = 0;
+        const btnLike = document.querySelector('#btn-like');
+        btnLike.addEventListener('click', () => {
+        console.log(btnLike);
+        contador ++;
+        document.getElementById('mostrar').innerHTML = contador;
+    })
+    }
+    likes();
 
-        //Likes publicación
-        const likes = () => {
-            let contador = 0;
-            const btnLike = document.querySelector('#btn-like');
-            btnLike.addEventListener('click', () => {
-            console.log(btnLike);
-            contador ++;
-            document.getElementById('mostrar').innerHTML = contador;
+    const deletePost = (id) => firebase.firestore().collection("publications").doc(id).delete();
+    const btnsDelete = document.querySelectorAll('.btn-delete');
+    btnsDelete.forEach(btn => {
+        btn.addEventListener('click', async (e) => {
+            //console.log(e.target.dataset.id);
+            await deletePost(e.target.dataset.id);
         })
-        }
+    });
 
-likes();
-        const deletePost = id => firebase.firestore().collection("publications").doc(id).delete();
-        const btnsDelete = document.querySelectorAll('.btn-delete');
-        btnsDelete.forEach(btn => {
-            btn.addEventListener('click', async (e) => {
-                //console.log(e.target.dataset.id);
-                await deletePost(e.target.dataset.id);
-            })
-        });
-
-
-        //Funcion editar
+    //Funcion editar
         const getPost = (id) => firebase.firestore().collection("publications").doc(id).get();
         const btnsEditions = document.querySelectorAll('.btn-edition');
         btnsEditions.forEach(btn => {
