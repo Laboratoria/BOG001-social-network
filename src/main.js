a77fe4dcc041eda016a73899dfbf33ee70d5bfc0
carolaverde <carolinalaverde5@gmail.com>
2020-09-17 09:46:38 -0500

    enrotando, index.js, main.js, index.routes,js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
+window.addEventListener('hashchange',() =>{
+console.log(window.location.hash)
 
-
-import {router} from './router/index.routes'
-//import { myFunction } from './lib/index.js';
-
-window.addEventListener('hashchange', () =>{
-    router(window.location.hash) //el windonw.location muetra la url
 })
--- src/router/index.routes.js
+++
@@ -1,7 +0,0 @@
-
-
-const router = (route) => {
-   console.log(route)
-}
-
-export {router}
