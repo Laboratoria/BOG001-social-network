b51cc40033e93e35c7b24455edb6ed08cf42a757
mayerly <mayerly3868@hotmail.com>
2020-09-24 11:52:05 -0500

    guadar carpeta

---
+++ src/router/index.routes.js
@@ -0,0 +1,9 @@
+const router = (route) => {
+    switch(route){
+        case "#/":
+           return console.log("hola")
+           case "#/login":
+               return console.log("vamos ahi")
+    }
+}
+export {router};
