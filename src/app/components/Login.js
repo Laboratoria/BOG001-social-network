ce7fc0c236c4f2a4fddca4013b90b432d0c3d895
carolaverde <carolinalaverde5@gmail.com>
2020-10-05 08:39:30 -0500

    cambios firebase

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,5 +1,5 @@
 // esta pagina es la segunda vista, formulario html
-
+import {createUser} from "./firebase.js"
 
 export function form (){
    const $form = document.createElement("div");
@@ -11,10 +11,10 @@ export function form (){
                 <h2>Iniciar Sesion</h2>
                 <form action="#" method="POST">
 
-                    <label for="usuario"></label>
-                    <input type ="text"placeholder="Usuario" id = "usuario" required>
+                    <label for="correoelec">Correo Electrónico *</label>
+                    <input type="email" id="correoelec" placeholder="correo@ejemplo.com" required>    
 
-                    <label for="contrasena"></label>
+                    <label for="contrasena">Contraseña *</label>
                     <input type ="password" placeholder ="Contraseña" id = "contrasena" required>
 
                     <label for="in-sesion"></label>
@@ -37,18 +37,20 @@ export function form (){
 
   function guardarFormulario(e){
        e.preventDefault();// esto me cancela el reinicio del formulario
-       let usuario = document.getElementById("usuario").value;
+       let mail = document.getElementById("correoelec").value;
        let contrasena = document.getElementById("contrasena").value;
 
-       console.log(usuario, contrasena);        
+       console.log(mail, contrasena);        
        
-      
+       createUser(mail, contrasena);
+       return createUser;
    }
 
        
     return $form;
     
 }
+
   
   
    
--- src/app/components/firebase.js
+++ src/app/components/firebase.js
@@ -1,22 +1,13 @@
-// Your web app's Firebase configuration
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDmzAmJMq1YRvEJGVZ43qWHmqezpOnt-B0",
-  authDomain: "new-style-a6dc8.firebaseapp.com",
-  databaseURL: "https://new-style-a6dc8.firebaseio.com",
-  projectId: "new-style-a6dc8",
-  storageBucket: "new-style-a6dc8.appspot.com",
-  messagingSenderId: "760891391735",
-  appId: "1:760891391735:web:e4045de7247eaa3cd41df2"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth(); // con esta constante le estoy diciendo al firebase que cuando le envie los datos quiero que los valide
-/*auth
-.createUserWithEmailAndPassword(usuario, contrasena)
-.then(credencialUsuario => {
-    console.log("vamos por buen camino")
-})*/
 
-export firebase
+export function createUser(email, password){
+   firebase.auth().createUserWithEmailAndPassword(email, password)
+   .then(userCredentials => {
+     userCredentials
+   })
+   .catch((error) => {
+    console.log(error);
+   })
+  console.log("desesperada")
+}
+
